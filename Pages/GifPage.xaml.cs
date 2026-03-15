319e7456008ba43d6a5ec92541e3d50680790c10
Louis Biard <louisbiard24@gmail.com>
2026-03-15 20:53:11 +0100

    Partie 2 fini

---
+++ Pages/GifPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace LouisApp.Pages;
+
+public partial class GifPage : ContentPage
+{
+    public GifPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnBackClicked(object sender, EventArgs e)
+    {
+        await Navigation.PopModalAsync();
+    }
+}
---
+++ Pages/HomePage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace LouisApp.Pages;
+
+public partial class HomePage : ContentPage
+{
+    public HomePage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnGifButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new GifPage());
+    }
+}
